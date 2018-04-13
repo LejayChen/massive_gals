68384b58bf134b1c934cec868fbbcddf9552bf2e
Lingjian Chen <fiorano1chen@gmail.com>
2018-04-13 14:27:44 -0300

    Delete cosmic_web.py

--- cosmic_web.py
+++
@@ -1,6 +0,0 @@
-from astropy.table import Table
-
-cat = Table.read('CUT_CLAUDS_HSC_VISTA_Ks23.3_PHYSPARAM_TM.fits')
-cat_gal = cat[cat['CLASS'] == 0]
-cat_massive_gal = cat_gal[cat_gal['MASS_BEST'] > 11.3]
-
