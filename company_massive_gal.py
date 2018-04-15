60feb27454d213aec44e6c65892f3981cf29f891
LejayChen@github.com <fiorano1chen@gmail.com>
2018-04-15 20:20:40 -0300

    Apr15

--- company_massive_gal.py
+++ company_massive_gal.py
@@ -15,7 +15,7 @@ cat_massive_gal = cat_gal[cat_gal['MASS_BEST'] > 11]
 
 def bkg(mass_cen, ra_central, cat_all_z_slice_rand, coord_massive_gal_rand):
     '''
-    return a background correction for each central galaxy by looking at black pointings without massive galaxies.
+    return a background correction for each central galaxy by looking at blank pointings without massive galaxies.
     mass_central: mass of the central galaxy,  ra_central: ra value for central galaxy. if ra_central>100, gal in COSMOS, if ra_central<100, gal in XMM-LSS
     '''
     # __init__
@@ -23,8 +23,7 @@ def bkg(mass_cen, ra_central, cat_all_z_slice_rand, coord_massive_gal_rand):
     mass_sat_rand = []
     mass_sat_sf_rand = []
     n = 0
-    num_p = 15  # number of blank pointing's
-
+    num_p = 10  # number of blank pointing's
     while n < num_p:  # get several blank pointing's to estimate background
         same_field = False  # find a random pointing in the same field as the central galaxy
         while not same_field:
@@ -39,7 +38,7 @@ def bkg(mass_cen, ra_central, cat_all_z_slice_rand, coord_massive_gal_rand):
                 same_field = False
 
         idx, sep2d, dist3d = match_coordinates_sky(SkyCoord(ra_rand, dec_rand, unit="deg"), coord_massive_gal_rand, nthneighbor=1)
-        if sep2d.degree > 2.5/dis/np.pi*180:  # make sure the random pointing is away from any central galaxy (blank)
+        if sep2d.degree > 1.2/dis/np.pi*180:  # make sure the random pointing is away from any central galaxy (blank)
             # cut all objects catalog to get neighbor catalog
             coord_all_z_slice_rand = SkyCoord(cat_all_z_slice_rand['RA']*u.deg, cat_all_z_slice_rand['DEC']*u.deg)
             coord_rand = SkyCoord(ra_rand*u.deg, dec_rand*u.deg)
@@ -58,7 +57,6 @@ def bkg(mass_cen, ra_central, cat_all_z_slice_rand, coord_massive_gal_rand):
             # calculate satellite counts in mass bins in blank pointing's
             count_gal_rand, edges_rand = np.histogram(10 ** (mass_neighbors_rand - mass_cen), np.arange(0, 1.01, 0.1))
             counts_gals_rand += count_gal_rand
-
             n = n + 1
 
     # stats
@@ -78,11 +76,11 @@ for z in np.arange(3, 20, 1)/10.:
     density = 0.00003  # desired constant (cumulative) volume number density (Mpc^-3)
     num = int(density * survey_v)
 
+    # slice catalog in redshift bin
     cat_massive_z_slice = cat_massive_gal[abs(cat_massive_gal['ZPHOT'] - z) < 0.1]  # massive galaxies in this z slice
     cat_massive_z_slice.sort('MASS_BEST')
     cat_massive_z_slice.reverse()
     cat_massive_z_slice = cat_massive_z_slice[:num]  # select most massive ones (keep surface density constant in different redshift bins)
-
     cat_all_z_slice = cat_gal[abs(cat_gal['ZPHOT'] - z) < 0.2]  # all galaxies in this z slice (large than delta z, will be cut further)
 
     # Fetch coordinates for massive gals
@@ -94,18 +92,14 @@ for z in np.arange(3, 20, 1)/10.:
     # __init__
     counts_gals = np.zeros(10)
     massive_counts = len(cat_massive_z_slice)
-    SMF_z = np.zeros(10)
-    SMF_z_bkg = np.zeros(10)
     mass_sat = []  # contains mass of sats
     mass_sat_sf = []
     cylinder_length = 1.5 * 0.03 * (1 + z)  # half cylinder height centered at one central galaxy
     isolated_counts = 0  # massive gals with no detected companions
     counts_gals_z = []  # pair counts for each massive gal
     for gal in cat_massive_z_slice:
-        # cut the z_slice catalog to get neighbors catalog
-        coord_gal = SkyCoord(gal['RA'] * u.deg, gal['DEC'] * u.deg)
-
-        cat_neighbors_z_slice = cat_all_z_slice[abs(cat_all_z_slice['ZPHOT'] - gal['ZPHOT']) < cylinder_length]
+        coord_gal = SkyCoord(gal['RA'] * u.deg, gal['DEC'] * u.deg)  # coordinate of the massive galaxy
+        cat_neighbors_z_slice = cat_all_z_slice[abs(cat_all_z_slice['ZPHOT'] - gal['ZPHOT']) < cylinder_length]  # cut the z_slice catalog to get neighbors catalog
         coord_neighbors_z_slice = SkyCoord(cat_neighbors_z_slice['RA'] * u.deg, cat_neighbors_z_slice['DEC'] * u.deg)
         cat_neighbors = cat_neighbors_z_slice[coord_neighbors_z_slice.separation(coord_gal).degree < 0.6/dis/np.pi*180]
         cat_neighbors = cat_neighbors[cat_neighbors['ID'] != gal['ID']]  # exclude central gal itself
@@ -115,42 +109,28 @@ for z in np.arange(3, 20, 1)/10.:
             isolated_counts += 1
             continue
 
-        # retrieve mass info in sat catalog (all, sf & q)
-        mass_neighbors = cat_neighbors['MASS_BEST']
+        mass_neighbors = cat_neighbors['MASS_BEST']  # retrieve mass info in sat catalog (all, sf & q)
         if gal['MASS_BEST'] < max(cat_neighbors['MASS_BEST']):  # exclude central gals which has larger mass companion
             massive_counts -= 1
             continue
+
         mass_neighbors_sf = cat_neighbors[cat_neighbors['SSFR_BEST'] > -11]['MASS_BEST']
         mass_central = gal['MASS_BEST']
 
-        # ### satellite counts in mass bins
+        # ### SATELLITE COUNTS in mass bins
         # background subtracted
-
         count_gal, edges = np.histogram(10**(mass_neighbors - mass_central), np.arange(0, 1.01, 0.1))
         mass_neighbors_bkg, count_gal_bkg, mass_sat_bkg, mass_sat_sf_bkg = bkg(mass_central, gal['RA'], np.copy(cat_neighbors_z_slice), coord_massive_gal)
         counts_gals = counts_gals + (np.array(count_gal, dtype='float64') - count_gal_bkg)  # total sat_gal counts (binned with mass fraction)
         counts_gals_z.append(sum(np.array(count_gal, dtype='float64') - count_gal_bkg))
-
-        SMF_z += np.histogram(mass_neighbors, bins=10, range=(10., 12.))[0]
-        SMF_z_bkg += np.histogram(mass_neighbors_bkg, bins=10, range=(10., 12.))[0]
 
         # ### total mass of satellites (all, sf & q; all=sf+q)
         # background subtracted
         mass_sat.append(np.sum(10 ** (mass_neighbors[mass_neighbors > 10] - 8)) - mass_sat_bkg)  # unit 10**8 M_sun
         mass_sat_sf.append(np.sum(10 ** (mass_neighbors_sf[mass_neighbors_sf > 10] - 8)) - mass_sat_sf_bkg)  # unit 10**8 M_sun
 
-    # ##################################  PLOT STELLAR MASS FUNCTIONS
-    # plt.figure(figsize=(7, 6))
-    # plt.rc('font', family='serif'), plt.rc('xtick', labelsize=15), plt.rc('ytick', labelsize=15)
-    # plt.plot(np.arange(10, 12, 2./10), SMF_z, '-o', alpha=0.4)
-    # plt.plot(np.arange(10, 12, 2./10), SMF_z_bkg, '-o', alpha=0.4)
-    # plt.plot(np.arange(10, 12, 2. / 10), SMF_z - SMF_z_bkg, '-o')
-    # plt.xlabel('mass', fontsize=14)
-    # plt.ylabel('number density', fontsize=14)
-    # plt.savefig('SMFs/smf_'+str(z)+'.png')
-    # plt.close()
-
-    # ################################ PLOT PAIR COUNTS DISTRIBUTION
+    # ######## PLOT ##################################
+    #  plot pair counts distribution
     plt.figure(figsize=(7, 6))
     plt.hist(counts_gals_z, histtype='step')
     plt.xlabel('Total number of companions per Central', fontsize=14)
@@ -166,12 +146,10 @@ for z in np.arange(3, 20, 1)/10.:
     total_mass_sat = np.mean(bootsresult_sat)
     total_mass_sat_sf = np.mean(bootsresult_sat_sf)
     total_mass_sat_q = total_mass_sat - total_mass_sat_sf
-
     std_mass_sat_total = np.std(bootsresult_sat)
     std_mass_sat_sf_total = np.std(bootsresult_sat_sf)
     std_mass_sat_q_total = np.sqrt(std_mass_sat_total ** 2 + std_mass_sat_sf_total ** 2)
 
-    # ######## PLOT #########
     # plot sat counts against mass fraction M/M_central
     np.set_printoptions(precision=2)
     fig = plt.figure(figsize=(7, 6))
@@ -187,6 +165,7 @@ for z in np.arange(3, 20, 1)/10.:
     plt.ylim([0.0005, 10])
     plt.savefig('companion_counts_plots/'+str(round(z,1))+'.png')
     plt.close()
+    ##################################################
 
     # output total sat masses to file
     total_mass_sat_log.write(str(z) + ' ' + str(round(total_mass_sat)) +
@@ -198,5 +177,4 @@ for z in np.arange(3, 20, 1)/10.:
                              ' '+str(massive_counts)+
                              ' '+str(isolated_counts)+
                              '\n')
-
 total_mass_sat_log.close()
--- radial.py
+++ radial.py
@@ -8,19 +8,19 @@ import numpy as np
 
 cat = Table.read('CUT2_CLAUDS_HSC_VISTA_Ks23.3_PHYSPARAM_TM.fits')
 cat_gal = cat[cat['CLASS'] == 0]
-cat_massive_gal = cat_gal[cat_gal['MASS_BEST'] > 10.9]
+cat_massive_gal = cat_gal[cat_gal['MASS_BEST'] > 11.0]
 
 
 def bkg(ra_central, cat_all_z_slice_rand, coord_massive_gal_rand):
     '''
-    return a background correction for each central galaxy by looking at black pointings without massive galaxies.
+    return a background correction for each central galaxy by looking at blank pointings without massive galaxies.
     mass_central: mass of the central galaxy,  ra_central: ra value for central galaxy. if ra_central>100, gal in COSMOS, if ra_central<100, gal in XMM-LSS
     '''
 
     # __init__
-    counts_gals_rand = np.zeros(100)
+    counts_gals_rand = np.zeros(19)
     n = 0
-    num_p = 5  # number of blank pointing's
+    num_p = 3  # number of blank pointing's
     while n < num_p:  # get several blank pointing's to estimate background
         same_field = False  # find a random pointing in the same field as the central galaxy
         while not same_field:
@@ -35,7 +35,7 @@ def bkg(ra_central, cat_all_z_slice_rand, coord_massive_gal_rand):
                 same_field = False
 
         idx, sep2d, dist3d = match_coordinates_sky(SkyCoord(ra_rand, dec_rand, unit="deg"), coord_massive_gal_rand, nthneighbor=1)
-        if sep2d.degree > 2.5/dis/np.pi*180:  # make sure the random pointing is away from any central galaxy (blank)
+        if sep2d.degree > 1.0/dis/np.pi*180:  # make sure the random pointing is away from any central galaxy (blank)
             # cut all objects catalog to get neighbor catalog
             coord_all_z_slice_rand = SkyCoord(cat_all_z_slice_rand['RA']*u.deg, cat_all_z_slice_rand['DEC']*u.deg)
             coord_rand = SkyCoord(ra_rand * u.deg, dec_rand * u.deg)
@@ -45,19 +45,16 @@ def bkg(ra_central, cat_all_z_slice_rand, coord_massive_gal_rand):
 
             coord_rand = SkyCoord(ra_rand * u.deg, dec_rand * u.deg)
             radius_neighbors_rand = coord_neighbors.separation(coord_rand).degree / 180. * np.pi * dis * 1000
-            count_gal_rand, edges_rand = np.histogram(radius_neighbors_rand, bins=10**np.linspace(1, 2.75, num=100))
+            count_gal_rand, edges_rand = np.histogram(radius_neighbors_rand, bins=10**np.linspace(1, 2.75, num=20))
             counts_gals_rand += count_gal_rand
-
             n = n + 1
 
     return counts_gals_rand/float(num_p)
 
-for z in np.arange(5, 5.1)/10.:
+for z in np.arange(7, 7.1)/10.:
     print('============='+str(z)+'================')
-    dis = WMAP9.angular_diameter_distance(z).value  # angular diameter distance at redshift z
-
     cat_massive_z_slice = cat_massive_gal[abs(cat_massive_gal['ZPHOT'] - z) < 0.3]
-    cat_all_z_slice = cat_gal[abs(cat_gal['ZPHOT'] - z) < 0.5]
+    cat_all_z_slice = cat_gal[abs(cat_gal['ZPHOT'] - z) < 0.6]
 
     # Fetch coordinates for massive gals
     cat_massive_z_slice['RA'].unit = u.deg
@@ -69,8 +66,10 @@ for z in np.arange(5, 5.1)/10.:
     radius_max = 600
     massive_counts = len(cat_massive_z_slice)
     for gal in cat_massive_z_slice:
+        print(gal['ID'])
+        dis = WMAP9.angular_diameter_distance(gal['ZPHOT']).value
         coord_gal = SkyCoord(gal['RA'] * u.deg, gal['DEC'] * u.deg)
-        cat_neighbors_z_slice = cat_all_z_slice[abs(cat_all_z_slice['ZPHOT'] - gal['ZPHOT']) < 0.5]
+        cat_neighbors_z_slice = cat_all_z_slice[abs(cat_all_z_slice['ZPHOT'] - gal['ZPHOT']) < 0.6]
         coord_neighbors_z_slice = SkyCoord(cat_neighbors_z_slice['RA'] * u.deg, cat_neighbors_z_slice['DEC'] * u.deg)
         cat_neighbors = cat_neighbors_z_slice[coord_neighbors_z_slice.separation(coord_gal).degree < 0.7 / dis / np.pi * 180]
         cat_neighbors = cat_neighbors[cat_neighbors['ID'] != gal['ID']]
@@ -85,7 +84,7 @@ for z in np.arange(5, 5.1)/10.:
 
         coord_neighbors = SkyCoord(cat_neighbors['RA'] * u.deg, cat_neighbors['DEC'] * u.deg)
         radius_list = coord_neighbors.separation(coord_gal).degree / 180. * np.pi * dis * 1000  # kpc
-        count_binned, bin_edges = np.histogram(radius_list, bins=10**np.linspace(1, 2.75, num=100))
+        count_binned, bin_edges = np.histogram(radius_list, bins=10**np.linspace(1, 2.75, num=20))
         bin_left_edges = bin_edges[:-1]
 
         sat_counts = np.array(count_binned, dtype='f8') - bkg(gal['RA'], cat_all_z_slice, coord_massive_gal)  # counts
@@ -94,12 +93,12 @@ for z in np.arange(5, 5.1)/10.:
         areas = np.array([])
         for i in range(len(bin_edges[:-1])):
             areas = np.append(areas, (bin_edges[i + 1] ** 2 - bin_edges[i] ** 2) * np.pi)
-
         radial_dist += sat_counts / areas
         radial_dist_err += sat_counts_err / areas
 
     plt.plot(bin_left_edges, radial_dist/float(massive_counts))
-    plt.annotate(str(massive_counts), (1, 1),color='red', fontsize=14, xycoords='axes points')
+    plt.annotate(str(massive_counts), (1, 1), color='red', fontsize=14, xycoords='axes points')
     plt.xscale('log')
     plt.yscale('log')
+    plt.savefig('radial_test.png')
     plt.show()
